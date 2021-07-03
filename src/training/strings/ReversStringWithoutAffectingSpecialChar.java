d5db5e7a83df3db03539ffeef2b099b0f5b0b6ad
Yacine Chakour <yacine.veg@gmail.com>
2021-07-03 02:26:52 -0400

    Practice

---
+++ src/training/strings/ReversStringWithoutAffectingSpecialChar.java
@@ -0,0 +1,52 @@
+package training.strings;
+
+public class ReversStringWithoutAffectingSpecialChar {
+
+    public static void main(String[] args) {
+        String special = "a,b$c";
+        String special2 = "vfg+plsd-/mnc,";
+
+        System.out.println("Original string " + special);
+        System.out.println("Reversed without changing special character " + reverseWithoutCHangingSpecialChars(special));
+
+        System.out.println("Original string " + special2);
+        System.out.println("Reversed without changing special character 2 " + reverseWithoutCHangingSpecialCharsSecondMethod(special2));
+
+    }
+
+    private static String reverseWithoutCHangingSpecialChars(String special) {
+
+        char[] original = special.toCharArray();
+        char[] reversed = new StringBuilder(special).reverse().toString().toCharArray();
+
+        for (int i = 0; i < original.length; i++) {
+            if (!Character.isAlphabetic((original[i]))) {
+                reversed[i] = original[i];
+            }
+        }
+
+        return String.valueOf(reversed);
+    }
+
+    private static String reverseWithoutCHangingSpecialCharsSecondMethod(String special) {
+        char[] chars = special.toCharArray();
+        int l = 0 , r = chars.length - 1;
+
+        while (l < r) {
+            if (!Character.isAlphabetic(chars[l])) {
+                l++;
+            } else if (!Character.isAlphabetic(chars[r])){
+                r--;
+            } else {
+                char aChar = chars[l];
+                chars[l] = chars[r];
+                chars[r] = aChar;
+                l++;
+                r--;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+
+}
