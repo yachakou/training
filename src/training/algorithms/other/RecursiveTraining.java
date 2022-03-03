0a0ff1b19cca15888f31a0eaba52b4071b4bf2b2
Yacine Chakour <ychakour@pbsc.com>
2022-03-03 11:18:26 -0500

    Practice

--- src/training/algorithms/other/RecursiveTraining.java
+++ src/training/algorithms/other/RecursiveTraining.java
@@ -4,6 +4,63 @@ public class RecursiveTraining {
 
     public static int factorial(int n) {
         if (n == 0) return 1;
+        System.out.println(n);
         return n * factorial(n - 1);
     }
+
+    public static char[] reverseString(char[] s) {
+
+        internalReverseString(s, 0, s.length - 1);
+        return s;
+    }
+
+    private static char[] internalReverseString(char[] s, int firstIndex, int lastIndex) {
+        if (firstIndex == s.length / 2) {
+            return s;
+        }
+        char temp = s[firstIndex];
+        s[firstIndex] = s[lastIndex];
+        s[lastIndex] = temp;
+        return internalReverseString(s, firstIndex + 1, lastIndex - 1);
+    }
+
+//    private static String internalReverseString(int index, char[] s) {
+//        if (index == 0) {
+//            return String.valueOf(s[index]);
+//        }
+//        return s[index] + internalReverseString(index - 1, s);
+//    }
+
+
+    //     Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = head;
+        head = head.next;
+        temp.next = head.next;
+        head.next = temp;
+        if (head.next.next != null) {
+            head.next.next = swapPairs(head.next.next);
+        }
+        return head;
+
+    }
+
+
 }
--- test/training/algorithms/other/RecursiveTrainingTest.java
+++ test/training/algorithms/other/RecursiveTrainingTest.java
@@ -1,6 +1,7 @@
 package training.algorithms.other;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,10 +9,43 @@ class RecursiveTrainingTest {
 
 
     @ParameterizedTest
-    @CsvSource(value = {"3,6", "4,24", "10,3628800"})
+    @CsvSource(value = {"3,6", "4,24", "10,3628800", "5,120"})
     public void factorial(int value, int expected) {
         int factorial = RecursiveTraining.factorial(value);
         Assertions.assertEquals(expected, factorial);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"hello,olleh", "hannah,hannah",})
+    public void reverseString(String value, String expected) {
+
+        char[] result = RecursiveTraining.reverseString(value.toCharArray());
+
+        char[] expectedArray = expected.toCharArray();
+        for (int i = 0; i < expectedArray.length; i++) {
+            Assertions.assertEquals(expectedArray[i], result[i]);
+        }
+
+    }
+
+    @Test()
+    public void swapPairs() {
+        RecursiveTraining.ListNode listNode = new RecursiveTraining.ListNode(1,
+                new RecursiveTraining.ListNode(2,
+                        new RecursiveTraining.ListNode(3,
+                                new RecursiveTraining.ListNode(4))));
+
+        RecursiveTraining.ListNode result = RecursiveTraining.swapPairs(listNode);
+
+        int[] values = {2, 1, 4, 3};
+        int i = 0;
+        RecursiveTraining.ListNode current = result;
+        while (current != null) {
+            System.out.println(current.val);
+            Assertions.assertEquals(values[i], current.val);
+            current = current.next;
+            i++;
+        }
     }
 
 }
